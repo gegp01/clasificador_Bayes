27fcd284ccf5a15d5576f7215f0817d3bd30a3b4
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2020-11-16 05:56:58 -0600

    Create Rt_municipios.R

---
+++ docs/R/Rt_municipios.R
@@ -0,0 +1,8 @@
+# COÓDIGO DE R PARA ESTIMAR EL INREMENTO EN CONTAGIOS (Rt) DE ACUERDO CON EL PROTOCOLO DE LA ORGANIZACION MUNDIAL PARA LA SALUD (PHAO). Rt SE CALCULA CON EL ALGORITMO EpiEstim() PARA CADA MUNICIPIO, EXCEPTO AQUELLOS MUNICIPIOS EN LOS QUE SE SE TIENEN POCAS PRUEBAS ().
+# autor: Gabriel E. García Peña
+# Centro de Ciencias de la Complejidad, Universidad NAcional Autónoma de México
+#
+# ANTES DE INICIAR, SE REQUIREN LOS ARCHIVO NODOS.rds, que se genera con la instrucciónes: (1) y (2) en https://gegp01.github.io/clasificador_Bayes/ 
+#
+
+
