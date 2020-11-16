b609851bdc2c5939329df2380019f2d879dedeee
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2020-11-16 06:08:48 -0600

    Update Rt_municipios.R

--- docs/R/Rt_municipios.R
+++ docs/R/Rt_municipios.R
@@ -2,7 +2,60 @@
 # autor: Gabriel E. García Peña
 # Centro de Ciencias de la Complejidad, Universidad NAcional Autónoma de México
 #
-# ANTES DE INICIAR, SE REQUIREN LOS ARCHIVO NODOS.rds, que se genera con la instrucciónes: (1) y (2) en https://gegp01.github.io/clasificador_Bayes/ 
+# ANTES DE INICIAR, SE REQUIREN LOS ARCHIVO mun.rds, que se genera con la instrucciónes: (1) y (2) en https://gegp01.github.io/clasificador_Bayes/ 
 #
+
+
+# CALCULAR TASA DE CRECIMIENTO (INFECTADOS HOY/INFECTADOS AYER)
+      
+      x4<-table(covid$municipio_oficial[covid$infectado==1], covid$FECHA_SINTOMAS[covid$infectado==1])
+      
+
+# CALCULA Rt PARA TODO EL PAIS
+#  
+#  require(EpiEstim)
+#  t_start<-seq(2, length(colSums(x4))-10)
+#  t_end<-t_start + 10
+#  
+#  y<-estimate_R(colSums(x4), method="parametric_si"
+#                , config = make_config(list(
+#                  mean_si = 4.8
+#                  , std_si = 2.3
+#                  , t_start = t_start
+#                  , t_end = t_end))
+#  )
+#
+# FUNCIÓN PARA CALCULAR Rt en CADA MUNICIPIO.
+  f1<-function(x) {
+    y<-estimate_R(data.frame(x4[x,]), method="parametric_si"
+                  , config = make_config(list(
+                    mean_si = 4.8
+                    , std_si = 2.3
+                    , t_start = t_start
+                    , t_end = t_end
+                    ))
+                  )
+
+    mean(y[['R']][["Median(R)"]])
+  }
+
+  x<-c(1:nrow(x4))
+  Rt<-sapply(x, f1)
+  
+  names(Rt)<-rownames(x4)
+  
+  
+# INTEGRAR LOS DATOS AL ARCHIVO mun.rds,
+  
+      mun$Rt<-Rt[match(mun$MUN_OFICIA, names(Rt))]
+  
+      mun$Rt2<-ifelse(mun$infectados+mun$negativos >= 20, mun$Rt, NA) # eliminamos los estimados de Rt con pocos casos
+  
+      mun$ID<-paste(mun$NOM_MUN, mun$NOM_ENT, mun$MUN_OFICIA, sep="-")
+      
+      mun$FECHA_ACTUALIZACION<-rep(covid$FECHA_ACTUALIZACION[1], nrow(mun))
+
+      saveRDS(mun.rds)
+      
 
 
