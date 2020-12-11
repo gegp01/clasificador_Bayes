9a6b757bcf84b9436245728afe38a53feb2503f5
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2020-12-11 09:58:50 -0600

    Update kioscos_cdmx.R

--- docs/R/kioscos_cdmx.R
+++ docs/R/kioscos_cdmx.R
@@ -1,6 +1,7 @@
 # Alojar el archivo en shiny-server/R/
 system("// hoy=`date '+%d_%m_%Y'`;
 	hoy=`date '+%d_%m_%Y'`;
-	wget -c 'https://datos.cdmx.gob.mx/explore/dataset/colonias-de-atencion-prioritaria-covid-kioscos/download/?format=geojson&timezone=America/Mexico_City&lang=es' -O kioscos_cdmx/kioscos_cdmx_$hoy.geojson")
+	wget -c 'https://datos.cdmx.gob.mx/explore/dataset/colonias-de-atencion-prioritaria-covid-kioscos/download/?format=geojson&timezone=America/Mexico_City&lang=es' -O kioscos_cdmx.geojson;
+	cp kioscos_cdmx.geojson kioscos_cdmx/kioscos_cdmx_$hoy.geojson")
   
   
